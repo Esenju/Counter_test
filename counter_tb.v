7920e9afde5f725e09e545bf6d0eea2f36829cf9
Peter Mbua <92553386+Esenju@users.noreply.github.com>
2022-10-17 00:54:14 -0400

    Add files via upload

---
+++ counter_tb.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+
+//testbench module.
+module counter_tb;
+
+    reg clk, reset, up_down_sw;
+    wire [3:0] count;
+    
+    //instanntiating counter design under test
+    up_down_counter dut(
+           .clk (clk), 
+           .reset(reset), 
+           .count(count),
+           .up_down_sw(up_down_sw));  //mapping test signals to counter inputs/outputs (ALL)
+    
+    //clock stimulus (start clock at active low, turn on and repeat alteration every 2 time-unit)
+   always 
+    #5 clk <= ~clk; // flip clock every 5ps 
+    
+   initial 
+   begin
+      
+        clk <= 1'b0; 
+        reset <= 1'b0;
+        
+    //input stimuli 
+        #10  reset <= 1'b1;   // assert reset for 5 time-unit (TU),
+        #100 reset <= 1'b0;   // then deactivate reset to low for 50 TUs
+      
+        #100 up_down_sw <= 1'b1;    //stimutate count up for 20TUs then count down
+        #100 up_down_sw <= 1'b0; 
+    end
+endmodule
+    
---
+++ up_down_coubter.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Peter Mbua
+// 
+// Create Date: 10/15/2022 08:15:28 AM
+// Design Name: un_down Counter
+// Module Name: up_down_coubter
+// Project Name: Training
+// Target Devices: Nexys 4
+// Tool Versions: 2021.1
+// Description: The counter is triggered by a clock and a sync reset. 
+                //one switch with alternating binary values is used to 
+                //make the counter count up or down as needed.
+// 
+// Dependencies: 
+// 
+// Revision: 22.01
+// Revision 0.01 - File Created
+// Additional Comments: synchrounous reset used, clock divider used too to make the counter practical.
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module up_down_counter(
+    input clk, 
+    input reset,
+    input up_down_sw,
+    //input load,
+   // input [3:0] data,
+    output reg [3:0] count
+    );
+    // this design is for simulation ONLY, as such no clock divider is necessary. 
+
+always@(posedge clk) 
+begin
+    if (reset)         //set cuounter to 0
+    count <= 0;   
+    else if(up_down_sw)      // upward counting
+        count <= count + 1;
+    else                     //downward counting 
+        count <= count - 1;
+ end
+endmodule
